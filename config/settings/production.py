47a3b7e528c3ec45cb82c09855b885bc46069a01
bryanadams <bryanadams@gmail.com>
2025-11-19 16:35:02 -0500

    Rerunning CI to try to pick up environment variables

--- config/settings/production.py
+++ config/settings/production.py
@@ -77,7 +77,7 @@ if "DJANGO_AWS_ACCESS_KEY_ID" in env.ENVIRON:
     # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
     AWS_SECRET_ACCESS_KEY = env("DJANGO_AWS_SECRET_ACCESS_KEY")
     # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-    AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
+    AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)  # type: ignore[call-overload]
 
 AWS_STORAGE_BUCKET_NAME = env("DJANGO_AWS_STORAGE_BUCKET_NAME")
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings

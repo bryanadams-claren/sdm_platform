47f531950c104cd0020ed400517a86e4f32c8b95
bryanadams <bryanadams@gmail.com>
2025-11-21 11:00:08 -0500

    Fixing the setting of the environment variables

--- config/settings/aws_local.py
+++ config/settings/aws_local.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import boto3
 import environ
@@ -15,7 +16,10 @@ def load_ssm_secrets(env: environ.Env, prefix: str = "/myapp"):
     Example:
         /myapp/DJANGO_SECRET_KEY -> os.environ['DJANGO_SECRET_KEY']
     """
-    ssm = boto3.client("ssm", region_name=env("AWS_REGION", default="us-east-2"))  # pyright: ignore[reportArgumentType]
+    ssm = boto3.client(
+        "ssm",
+        region_name=env("AWS_PARAMETER_REGION", default="us-east-2"),  # pyright: ignore[reportArgumentType]
+    )
     try:
         # Fetch parameters by path
         paginator = ssm.get_paginator("get_parameters_by_path")
@@ -25,7 +29,7 @@ def load_ssm_secrets(env: environ.Env, prefix: str = "/myapp"):
             for param in page.get("Parameters", []):
                 # Strip the prefix and any leading slash
                 env_key = param["Name"].replace(prefix, "").lstrip("/").upper()
-                env(env_key, param["Value"])
+                os.environ[env_key] = param["Value"]
     except ClientError:
         errmsg = "Error loading SSM parameters"
         logger.exception(errmsg)

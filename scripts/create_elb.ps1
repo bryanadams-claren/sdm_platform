5190f852c494d733ea1e4dcf89139f3cab09cfeb
bryanadams <bryanadams@gmail.com>
2025-11-22 14:34:14 -0500

    Slight adjustment to the extension to make the load balancer listen on https

---
+++ scripts/create_elb.ps1
@@ -0,0 +1,21 @@
+# Create Elastic Beanstalk environment with load balancer
+param(
+    [string]$EnvName = "sdm-platform-web-dev2"
+)
+
+uv run eb create $EnvName `
+  --instance_type t3.small `
+  --vpc.id vpc-0ea9b7f4341e4121e `
+  --vpc.ec2subnets subnet-0d8fdf6b575253e49,subnet-036659884bdb99a58 `
+  --vpc.elbsubnets subnet-0d8fdf6b575253e49,subnet-036659884bdb99a58 `
+  --vpc.elbpublic `
+  --vpc.publicip `
+  --vpc.securitygroups sg-01980717819d4a489 `
+  --instance_profile aws-elasticbeanstalk-ec2-role `
+  --elb-type application `
+  --scale 1
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Environment $EnvName created successfully!" -ForegroundColor Green
+    Write-Host "Check status with: uv run eb status $EnvName"
+}

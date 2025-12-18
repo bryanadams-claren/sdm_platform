dc8e0c168f23202b9424afb8cd7cf5c3cfe4feeb
bryanadams <bryan.adams@clarenhealth.com>
2025-12-18 11:06:28 -0500

    Updating the production site; adding new env variables

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,6 +23,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS",
     default=[  # pyright: ignore[reportArgumentType]
+        ".clarenhealth.com",
         ".perspicacioushealth.com",
         ".elasticbeanstalk.com",
         ".backpaindecisionsupport.com",

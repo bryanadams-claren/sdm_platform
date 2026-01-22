61eacc8827de61285ab32671cc936f0b7a4a713b
bryanadams <bryan.adams@clarenhealth.com>
2026-01-22 09:52:09 -0500

    Adding the SES region, fixed the cert to use clarenhealth

--- config/settings/production.py
+++ config/settings/production.py
@@ -180,7 +180,11 @@ INSTALLED_APPS += ["anymail"]
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
 EMAIL_BACKEND = "anymail.backends.amazon_ses.EmailBackend"
-ANYMAIL = {}
+ANYMAIL = {
+    "AMAZON_SES_CLIENT_PARAMS": {
+        "region_name": "us-east-2",
+    },
+}
 
 # django-compressor
 # ------------------------------------------------------------------------------
